d5ad9e2965cddf21ac5ae490efef32559d3a6cdd
Dan Büschlen <danbueschlen@gmail.com>
2019-09-28 20:41:46 +0200

    move data to a separate file

--- src/index.js
+++ src/index.js
@@ -1,56 +1,5 @@
 import './styles/index.scss'
-
-let emotions = [
-  "enttäuscht",
-  "gespannt",
-  "angespannt",
-  "vorfreudig",
-  "überrascht",
-  "stolz",
-  "gelangweilt",
-  "zuversichtlich",
-  "neugierig",
-  "belustigt",
-  "neugierig",
-  "eifersüchtig",
-  "verletzt",
-  "beleidigt",
-  "aggressiv",
-  "zornig",
-  "gleichgültig",
-  "angriffslustig",
-  "emotional",
-  "verspielt",
-  "geschmeichelt",
-  "überlegen",
-  "erstaunt",
-  "beglückt",
-  "unaufgeregt",
-  "verbittert",
-  "zufrieden",
-  "ängstlich",
-  "schüchtern",
-  "aufbrausend",
-]
-
-let objects = [
-  "altes Telefon",
-  "Schaufensterpuppe",
-  "Snowboardschuh",
-  "Lederjacke",
-  "Lupe",
-  "Armreif",
-  "Ohrringe",
-  "Plastik-Flamingo",
-  "pinke Sonnenbrille",
-  "Trinkflasche",
-  "Gitarre",
-  "Porzellan",
-  "Spiegel",
-  "Glasvase",
-  "Koffer",
-  "Radio",
-]
+import {emotions, objects} from "./js/data.js"
 
 
 let emotionEl = document.querySelector("[data-value-emotion]")
