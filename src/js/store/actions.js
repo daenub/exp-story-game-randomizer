bb54c466d6b8dddb1b7d84ec37f8c317245928f9
Dan Büschlen <danbueschlen@gmail.com>
2019-10-05 20:19:57 +0200

    shuffle order

--- src/js/store/actions.js
+++ src/js/store/actions.js
@@ -1,23 +1,29 @@
-// function shuffle(a) {
-//   console.log(a)
-//   for (let i = a.length - 1; i > 0; i--) {
-//     const j = (Math.floor(Math.random() * (i + 1))[(a[i], a[j])] = [a[j], a[i]])
-//   }
-//   return a
-// }
+function shuffle(a) {
+  let j, x, i
+  let array = a.concat()
+
+  for (i = array.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1))
+    x = array[i]
+    array[i] = array[j]
+    array[j] = x
+  }
+
+  return array
+}
 
 export function initNewGame() {
   return (dispatch, getState) => {
     const {emotions, objects, places} = getState()
 
     let emotionsOrder = emotions.map((e, i) => i)
-    // emotionsOrder = shuffle(emotionsOrder)
+    emotionsOrder = shuffle(emotionsOrder)
 
     let objectsOrder = objects.map((o, i) => i)
-    // objectsOrder = shuffle(objectsOrder)
+    objectsOrder = shuffle(objectsOrder)
 
     let placesOrder = places.map((p, i) => i)
-    // placesOrder = shuffle(placesOrder)
+    placesOrder = shuffle(placesOrder)
 
     dispatch({
       type: "SET_EMOTIONS_ORDER",
