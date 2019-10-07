f31c5561a61fca59cd1b7b67679a5ce6635425b2
Dan Büschlen <danbueschlen@gmail.com>
2019-10-07 18:00:55 +0200

    just show the current turn and some context

--- src/js/Randomizer.js
+++ src/js/Randomizer.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useEffect, useRef} from "react"
 import {useSelector} from "react-redux"
 
 import {getCurrentTurn, orderedTurnList} from "./store/selectors.js"
@@ -7,20 +7,28 @@ export const Randomizer = () => {
   const currentTurn = useSelector(getCurrentTurn)
   const turns = useSelector(orderedTurnList)
 
+  const turnList = useRef()
+  const turnHeader = useRef()
+
+  useEffect(() => {
+    const rowHeight = turnHeader.current.offsetHeight
+    let turnMultiplicator = currentTurn - 1
+
+    turnList.current.style.transform = `translateY(${turnMultiplicator *
+      rowHeight *
+      -1}px)`
+  }, [currentTurn, turnList, turnHeader])
+
   return (
     <div className="randomizer">
+      <div className="turn-row turn-row--header" ref={turnHeader}>
+        <div className="turn-row__cell turn-row__cell--number">Zug</div>
+        <div className="turn-row__cell turn-row__cell--place">Ort</div>
+        <div className="turn-row__cell turn-row__cell--emotion">Emotion</div>
+        <div className="turn-row__cell turn-row__cell--object">Gegenstand</div>
+      </div>
       <div className="turn-list-wrapper">
-        <ul className="turn-list">
-          <li className="turn-row turn-row--header">
-            <div className="turn-row__cell turn-row__cell--number">Zug</div>
-            <div className="turn-row__cell turn-row__cell--place">Ort</div>
-            <div className="turn-row__cell turn-row__cell--emotion">
-              Emotion
-            </div>
-            <div className="turn-row__cell turn-row__cell--object">
-              Gegenstand
-            </div>
-          </li>
+        <ul className="turn-list" ref={turnList}>
           {turns.map(({turn, place, emotion, object}) => (
             <li
               className={`turn-row ${
