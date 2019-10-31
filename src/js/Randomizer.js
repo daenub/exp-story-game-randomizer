9d2c5cf86cbd07109295048ed07467df64a0d746
Dan Büschlen <danbueschlen@gmail.com>
2019-10-31 14:30:55 +0100

    use hard coded row height

--- src/js/Randomizer.js
+++ src/js/Randomizer.js
@@ -1,34 +1,23 @@
-import React, {useEffect, useRef} from "react"
+import React from "react"
 import {useSelector} from "react-redux"
 
 import {getCurrentTurn, orderedTurnList} from "./store/selectors.js"
+
+const ROW_HEIGHT = 41
 
 export const Randomizer = () => {
   const currentTurn = useSelector(getCurrentTurn)
   const turns = useSelector(orderedTurnList)
 
-  const turnList = useRef()
-  const turnHeader = useRef()
-
-  useEffect(() => {
-    const rowHeight = turnHeader.current.offsetHeight
-    let turnMultiplicator = currentTurn === 0 ? 0 : currentTurn - 1
-
-    turnList.current.style.transform = `translateY(${turnMultiplicator *
-      rowHeight *
-      -1}px)`
-  }, [currentTurn, turnList, turnHeader])
+  const turnMultiplicator = currentTurn === 0 ? 0 : currentTurn - 1
+  const turnListStyle = {
+    transform: `translateY(${turnMultiplicator * ROW_HEIGHT * -1}px)`,
+  }
 
   return (
     <div className="randomizer">
-      <div className="turn-row turn-row--header" ref={turnHeader}>
-        {/*<div className="turn-row__cell turn-row__cell--number">Zug</div>*/}
-        <div className="turn-row__cell turn-row__cell--place">Ort</div>
-        <div className="turn-row__cell turn-row__cell--object">Gegenstand</div>
-        <div className="turn-row__cell turn-row__cell--emotion">Emotion</div>
-      </div>
       <div className="turn-list-wrapper">
-        <ul className="turn-list" ref={turnList}>
+        <ul className="turn-list" style={turnListStyle}>
           {turns.map(({turn, place, emotion, object}) => (
             <li
               className={`turn-row ${
